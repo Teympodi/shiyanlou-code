6e1679319977c9948d7799c16e398ab9598ba243
Teym <teympodi@163.com>
2022-10-24 17:53:48 +0800

    frist commit

---
+++ var.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+a = input("How many apples do you have:")
+print("You have got " + a + " apples!")
+b = input("How many banana do you have:")
+print("You have got " + b + " bananas!")
+total = int(a) + int(b)
+print("You have got " + str(total) + " fruits in all!")
