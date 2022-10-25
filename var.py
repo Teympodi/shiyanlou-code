b625b07ddabb92b6b1dee545e93a315cb456d351
Teym <teympodi@163.com>
2022-10-25 10:36:38 +0800

    Add try() except()

--- var.py
+++ var.py
@@ -3,5 +3,8 @@ a = input("How many apples do you have:")
 print("You have got " + a + " apples!")
 b = input("How many banana do you have:")
 print("You have got " + b + " bananas!")
-total = int(a) + int(b)
-print("You have got " + str(total) + " fruits in all!")
+try:
+    total = int(a) + int(b)
+    print("You have got " + str(total) + " fruits in all!")
+except:
+    print("\033[41m[error]\033[0m -----Input numbers!!!")
